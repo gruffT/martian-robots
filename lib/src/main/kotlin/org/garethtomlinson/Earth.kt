29cafa7bfa6f240e23657a6c655a164e7bad9b1d
Gareth Tomlinson <garetht@garethtomlinson.co.uk>
2024-03-26 10:40:53 +0000

    refactor(Earth): re-order methods for clarity

--- lib/src/main/kotlin/org/garethtomlinson/Earth.kt
+++ lib/src/main/kotlin/org/garethtomlinson/Earth.kt
@@ -7,29 +7,6 @@ import org.garethtomlinson.reporting.Log
 
 class Earth {
     companion object {
-        fun mars(missionDetails: String): Mars {
-            val missionDetailLines = missionDetails.trim().split("\n").filter { line -> line.isNotEmpty() }
-            if (missionDetailLines.isEmpty()) throw BadConfigurationException("Mission Details", "No Mars configuration")
-            val marsConfiguration = missionDetailLines[0]
-            return Mars.from(marsConfiguration)
-        }
-
-        fun missions(
-            missionDetails: String,
-            mars: Mars,
-        ): List<Mission> {
-            val missionDetailsWithNoMars = missionDetails.trim().split("\n").drop(1).map { it.trim() }
-            val missionChunks = missionDetailsWithNoMars.chunked(3).map { chunk -> chunk.filter { line -> line != "" } }
-            if (missionDetails.trim().isEmpty() || missionChunks.isEmpty()) {
-                throw BadConfigurationException(
-                    "Missions",
-                    "No missions specified",
-                )
-            }
-            if (missionChunks.any { chunk -> chunk.size != 2 }) throw BadConfigurationException("Missions", "Input incorrectly chunked")
-            return missionChunks.map { chunk -> Mission.from(missionConfig = chunk, mars = mars) }
-        }
-
         fun expedition(
             missions: List<Mission>,
             mars: Mars,
@@ -54,6 +31,29 @@ class Earth {
                     statusStage.execute(instruction)
                 }
             return missionStatus
+        }
+
+        fun mars(missionDetails: String): Mars {
+            val missionDetailLines = missionDetails.trim().split("\n").filter { line -> line.isNotEmpty() }
+            if (missionDetailLines.isEmpty()) throw BadConfigurationException("Mission Details", "No Mars configuration")
+            val marsConfiguration = missionDetailLines[0]
+            return Mars.from(marsConfiguration)
+        }
+
+        fun missions(
+            missionDetails: String,
+            mars: Mars,
+        ): List<Mission> {
+            val missionDetailsWithNoMars = missionDetails.trim().split("\n").drop(1).map { it.trim() }
+            val missionChunks = missionDetailsWithNoMars.chunked(3).map { chunk -> chunk.filter { line -> line != "" } }
+            if (missionDetails.trim().isEmpty() || missionChunks.isEmpty()) {
+                throw BadConfigurationException(
+                    "Missions",
+                    "No missions specified",
+                )
+            }
+            if (missionChunks.any { chunk -> chunk.size != 2 }) throw BadConfigurationException("Missions", "Input incorrectly chunked")
+            return missionChunks.map { chunk -> Mission.from(missionConfig = chunk, mars = mars) }
         }
     }
 }
