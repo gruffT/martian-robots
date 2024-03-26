2856d28f35841de50c84b8a888d4a811343778d6
Gareth Tomlinson <garetht@garethtomlinson.co.uk>
2024-03-26 10:50:31 +0000

    refactor(Status): rename variables for clarity

--- lib/src/main/kotlin/org/garethtomlinson/Status.kt
+++ lib/src/main/kotlin/org/garethtomlinson/Status.kt
@@ -8,19 +8,21 @@ typealias RobotTrail = List<Robot>
 class Status private constructor(val missions: List<RobotTrail>, val mars: Mars) {
     fun execute(instruction: Instruction): Status {
         val previousMissions = missions.dropLast(1)
-        val previousLastLostPositions =
+        val previousLostPositions =
             previousMissions.map { mission -> mission.last() }
                 .filter { robot -> !mars.insideBounds(robot) }
 
         val currentMission = missions.last()
         val currentRobot = currentMission.last()
+
+        // Ignore commands to a lost Robot
         if (!mars.insideBounds(currentRobot)) return Status(missions = missions, mars = mars)
 
         val updatedRobot = currentRobot.execute(instruction)
 
         val updatedMission =
             currentMission +
-                if (robotIsColocatedWithLostRobot(previousLastLostPositions, updatedRobot)) {
+                if (robotIsColocatedWithLostRobot(previousLostPositions, updatedRobot)) {
                     currentRobot
                 } else {
                     updatedRobot
@@ -30,17 +32,15 @@ class Status private constructor(val missions: List<RobotTrail>, val mars: Mars)
     }
 
     private fun robotIsColocatedWithLostRobot(
-        previousLastLostPositions: List<Robot>,
+        previousLostPositions: List<Robot>,
         updatedRobot: Robot,
-    ) = previousLastLostPositions.any { robot -> updatedRobot.equivalent(robot) }
+    ): Boolean = previousLostPositions.any { robot -> updatedRobot.equivalent(robot) }
 
     fun missionReports(): List<MissionReport> {
         return missions.map { mission -> MissionReport(lastKnownPosition(mission), !mars.insideBounds(mission.last())) }
     }
 
-    private fun lastKnownPosition(mission: RobotTrail): Robot {
-        return mission.last { robot -> mars.insideBounds(robot) }
-    }
+    private fun lastKnownPosition(mission: RobotTrail): Robot = mission.last { robot -> mars.insideBounds(robot) }
 
     fun startNewMission(robot: Robot): Status {
         val robotTrail = listOf(robot)
